47330201c58ffb3dedfd67e92beba589c0c1a1c8
Calvin McMurray <calvin.r.mcm@gmail.com>
2018-02-27 10:11:23 -0700

    Compartmentalizing.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ class HeaderButton extends Component{
   render() {
     return (
       <li>
-          <a data-toggle="tab" className={this.props.active ? "active" : ""} href={this.props.ref} download>{this.props.name} <span className="sr-only">(current)</span></a>
+          <a data-toggle="tab" className={this.props.active} href={this.props.link} download>{this.props.name} <span className="sr-only">(current)</span></a>
       </li>
     )
   }
+}
 
+class HeaderDropdown extends Component{
+  render() {
+    return (
+    <li className="dropdown">
+      <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{this.props.name} <span className="caret"></span></a>
+    </li>
+    )
+  }
 }
 
 class Header extends Component {
@@ -20,18 +29,15 @@ class Header extends Component {
       <div className="container navbar-left">
           <a className="navbar-brand" data-toggle="tab" href="#about">Calvin</a>
           <ul className="nav navbar-nav">
-            <HeaderButton active="active" ref="#about" name="About"/>
-            <li>
-                <a data-toggle="tab" href="#pro" download>Hire Me <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="dropdown">
-              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Family History Tech Lab <span className="caret"></span></a>
+            <HeaderButton active="active" link="#about" name="About"/>
+            <HeaderButton active="" link="#pro" name="Hire Me"/>
+            <HeaderDropdown name="Family History Tech Lab">
               <ul className="dropdown-menu">
                 <li><a data-toggle="tab" href="https://fhtl.byu.edu">Family History Tech Lab Portal</a></li>
                 <li><a data-toggle="tab" href="https://recordquest.fhtl.byu.edu">Record Quest</a></li>
                 <li><a data-toggle="tab" href="https://virtual-pedigree.fhtl.byu.edu">Virtual Pedigree</a></li>
               </ul>
-            </li>
+            </HeaderDropdown>
           </ul>
       </div>
     </nav>
